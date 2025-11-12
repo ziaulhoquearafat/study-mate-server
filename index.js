d7823554d3698ee6eea4ac37c321cc3f94ab6570
Arafat <mdarafat3167@gmail.com>
2025-11-12 08:17:55 +0600

    deleted api implemented

--- index.js
+++ index.js
@@ -79,6 +79,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("partner-request/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await partnerRequestCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
